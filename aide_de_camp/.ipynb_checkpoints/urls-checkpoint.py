fc111efe2372ace1caa960af01e70bef99b83f33
Thufir Hawat <0.Thufir.0@gmail.com>
2021-11-17 16:55:59 +0100

    napoleon v5 shakos1

--- aide_de_camp/.ipynb_checkpoints/urls-checkpoint.py
+++ aide_de_camp/.ipynb_checkpoints/urls-checkpoint.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('napoleon_1806/', include('napoleon_1806.urls')),
+    path('', include('napoleon_1806.urls')),
 ]
