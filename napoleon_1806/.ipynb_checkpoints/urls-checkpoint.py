ed1f5958941f7218a480a6dc1d2b06e170856763
Thufir Hawat <0.Thufir.0@gmail.com>
2021-11-18 14:58:24 +0100

    napoleon v6 shakos2 mobile1

--- napoleon_1806/.ipynb_checkpoints/urls-checkpoint.py
+++ napoleon_1806/.ipynb_checkpoints/urls-checkpoint.py
@@ -7,3 +7,6 @@ app_name = 'napoleon_1806'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+urlpatterns += [
+    path('test', views.test, name='test'),
+]
--- napoleon_1806/views.py
+++ napoleon_1806/views.py
@@ -7,3 +7,9 @@ def index(request):
     
     return render(request, 'napoleon_1806/index.html', context)
     
+def test(request):
+    num_visits = request.session.get('num_visits', 0)
+    context = {'num_visits': num_visits}
+    
+    return render(request, 'napoleon_1806/test.html', context)
+    
