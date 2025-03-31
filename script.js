1a979a01ab1975fcc8acc1e978b5bfff0cfa726f
G Delvoye <gau.delvoye@gmail.com>
2025-03-31 17:17:31 +0200

    refactor: better design for mobile.

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ let dataChart =  {
     labels: ['France', 'Equality',  'Prussia'],
     datasets: [{
         data: expectedResult(nbDiceFrance, nbDicePrussia),
-        backgroundColor: ['#3399FF',  '#FF6600', '#888888'],
-        hoverBackgroundColor: ['#3399FF',  '#FF6600', '#888888'],
+        backgroundColor: ['#888888', '#FF6600', '#3399FF'],
+        hoverBackgroundColor: ['#888888', '#FF6600','#3399FF'],
     }]
 };
 
